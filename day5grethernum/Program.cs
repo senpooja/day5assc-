35cb95dc9aac13b26ecbcb8d1f058f8eb2380470
senpooja <poojasen744055@gmail.com>
2022-07-20 17:46:33 +0530

    added day5ass3 files

---
+++ day5grethernum/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("enter any 3 number :");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a == b && b == c)
+{
+    Console.WriteLine("a,b and c are equal : " + a + b + c );
+
+}
+else if (a > b  && a > c)
+{
+    Console.WriteLine("a is greter number : " + a);
+
+}
+else if (b > c && b > a)
+{
+    Console.WriteLine("b is greter number : " + b);
+
+}
+else
+{
+    Console.WriteLine("c is greter number :" + c);
+
+}
