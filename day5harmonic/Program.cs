62d681dcfef96346f82117d333034a3151dfa208
senpooja <poojasen744055@gmail.com>
2022-07-20 17:50:19 +0530

    added day5ass4 files

---
+++ day5harmonic/Program.cs
@@ -0,0 +1,14 @@
+﻿
+double result = 0, num = 0; 
+Console.WriteLine("enter your number :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{ 
+    num = num + (1 / i);
+
+  result = num;
+   // Console.WriteLine("the value of harmonic no. " + num + " is: " + result);
+}
+
+Console.WriteLine("the value of harmonic no. " + num + " is: " + result);
