921cfbce7eac2d485da25909dc5219c942f107b7
senpooja <poojasen744055@gmail.com>
2022-07-20 17:54:42 +0530

    added day5ass5 files

---
+++ day5leapyear/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("enter year ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+if ((year % 4) == 0)
+    
+   
+    {
+        Console.WriteLine("this is leap year");
+
+    }
+
+else if ((year % 400) == 0)
+    {
+        Console.WriteLine("this is leap year ");
+    }
+else
+        {
+            Console.WriteLine("this is not a leap year");
+        }
+    
+
