f21f494069bc0bdb6062f2658ca12fdae563121a
senpooja <poojasen744055@gmail.com>
2022-07-20 17:37:17 +0530

    added day5ass2 files

---
+++ day5evenodd/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("enter a number  :");
+int number  = Convert.ToInt32(Console.ReadLine());
+
+if (number == 0)
+{
+    Console.WriteLine("given number is even :" + number);
+
+}
+else if (number % 2 == 0)
+{
+    Console.WriteLine("given number is even : " + number);
+}
+else
+{
+    Console.WriteLine("given number is odd : " + number);
+}
