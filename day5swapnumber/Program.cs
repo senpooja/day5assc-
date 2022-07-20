914f823b8029269e9d53b40c1da81862184af9a3
senpooja <poojasen744055@gmail.com>
2022-07-20 18:03:53 +0530

    added day5ass6 files

---
+++ day5swapnumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 100;
+int b = 400;
+int temp;
+Console.WriteLine("befor swaping the value a : " + a + "    b : " + b);
+
+temp = a;
+a = b;
+b = temp;
+
+Console.WriteLine("after swaping the value a : " + a + "      b : " + b);
+    
